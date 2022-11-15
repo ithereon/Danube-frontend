3ce9564f47589fa6a609f9ef0751446a8b6f341c
smith <jamesmith110111@gmail.com>
2022-11-16 00:21:42 +0600

    sign out url fixed

--- components/Dropdowns/UserDropdown.js
+++ components/Dropdowns/UserDropdown.js
@@ -94,7 +94,8 @@ export default function UserDropdown(props) {
 				</MenuItem>
 				<MenuItem
 					sx={{'&:hover': {color: 'white',backgroundColor: '#6366f1'}}}
-					onClick={(e) => {e.preventDefault();signOut({callbackUrl: 'https://live-production-frontend.herokuapp.com/auth/login'});}}>
+					// onClick={(e) => {e.preventDefault();signOut({callbackUrl: '/auth/login'});}}>
+					onClick={(e) => {e.preventDefault();signOut();}}>
 					<ListItemIcon>
 						<Logout fontSize="small" />
 					</ListItemIcon>
