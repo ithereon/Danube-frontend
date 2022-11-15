6794a7cf407681fe52a65764922267545f32e1c4
smith <jamesmith110111@gmail.com>
2022-11-15 21:56:31 +0600

    contact info

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ const Home = (props) => {
 					<div className="container mx-auto items-center flex flex-wrap sm:flex-nowrap">
 						<div className="w-full py-4 px-4 md:w-6/12 lg:px-10">
 							<div className="max-w-[500px]">
-								<h3 className="font-semibold text-[2rem] md:text-[2rem] text-neutral-800 mb-5">
+								<h3 className="font-semibold text-[2rem] md:text-[2rem] text-neutral-800 mb-2">
 									Hi, Kean.<br/>
 									I am Katelyna. I have a problem for my upwork account.<br/>
 									It's blocked and I can't know that's why.<br/>
